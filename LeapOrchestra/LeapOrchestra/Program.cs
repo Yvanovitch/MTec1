fff0d21ecc75206034d1ec66f329dd9f6b899037
Yvanovitch <yvan.richer@isen-lille.fr>
2013-06-12 01:37:52 -0700

    UP 1

---
+++ LeapOrchestra/LeapOrchestra/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapOrchestra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
