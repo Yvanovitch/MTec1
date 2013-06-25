a5276b51f05713936e06ce3f05dbbd4e684deb38
Koniev <gautier.carree@isen-lille.fr>
2013-06-25 02:19:02 -0700

    change references

--- LeapOrchestra/LeapOrchestra/WPF/Graph.Designer.cs
+++ LeapOrchestra/LeapOrchestra/WPF/Graph.Designer.cs
@@ -29,20 +29,9 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
             this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(161, 391);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Refresh";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // zedGraphControl1
             // 
@@ -69,7 +58,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(390, 426);
             this.Controls.Add(this.zedGraphControl1);
-            this.Controls.Add(this.button1);
             this.Name = "Graph";
             this.Text = "Graph";
             this.Load += new System.EventHandler(this.Graph_Load);
@@ -79,7 +67,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
         private ZedGraph.ZedGraphControl zedGraphControl1;
         private System.Windows.Forms.Timer timer1;
     }
